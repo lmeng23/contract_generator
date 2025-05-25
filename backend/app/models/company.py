0c39533a64d7d0e866786b7a3bbbab14343a610d
Hill <lmeng@zknu.edu.cn>
2025-05-25 11:20:28 +0800

    修改时区问题

--- backend/app/models/company.py
+++ backend/app/models/company.py
@@ -15,7 +15,7 @@ class Company(Base):
     bank_account = Column(String(100))
     bank_address = Column(String(255))
     created_at = Column(DateTime(timezone=True),
-                        server_default=func.now().op('AT TIME ZONE')('Asia/Shanghai'))
+                        server_default=func.now())
     updated_at = Column(DateTime(timezone=True),
-                        onupdate=func.now().op('AT TIME ZONE')('Asia/Shanghai'),
-                        server_default=func.now().op('AT TIME ZONE')('Asia/Shanghai'))
+                        onupdate=func.now(),
+                        server_default=func.now())
